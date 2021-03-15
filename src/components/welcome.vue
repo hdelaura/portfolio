b2e1e51fefabbb17733a239acb6a3e6778d6f9cc
Haley DeLaura <d00266543@dmail.dixie.edu>
2021-03-15 17:11:54 -0600

    bulma

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,33 +1,18 @@
 <template>
 <div class="container">
   <section class="section">
-      <h1 class="title">
-        Bulma
-      </h1>
-
-      <p class="subtitle">
-        Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
-      </p>
-
-      <div class="field">
-        <div class="control">
-          <input class="input" type="text" placeholder="Input">
+      <div class="columns is-centered">
+        <div class="column is-full-mobile is-full-desktop">
+          <p class="title is-1 is-spaced">HELLO</p>
         </div>
       </div>
-
-      <div class="field">
-        <p class="control">
-          <span class="select">
-            <select>
-              <option>Select dropdown</option>
-            </select>
-          </span>
-        </p>
-      </div>
-
-      <div class="buttons">
-        <a class="button is-primary">Primary</a>
-        <a class="button is-link">Link</a>
+      <div class="columns">
+        <div class="column is-half">
+          <p class="title is-1 is-spaced">HELLO</p>
+        </div>
+        <div class="column is-half">
+          <p class="title is-1 is-spaced">HELLO</p>
+        </div>
       </div>
   </section>
 </div>
