2c57859716656e196a75ce4c7761ef2a88d35e08
Haley DeLaura <d00266543@dmail.dixie.edu>
2021-03-15 16:26:21 -0600

    Added router functionality and node sass

---
+++ src/components/welcome.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="container">
+  <section class="section">
+      <h1 class="title">
+        Bulma
+      </h1>
+
+      <p class="subtitle">
+        Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+      </p>
+
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Input">
+        </div>
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div class="buttons">
+        <a class="button is-primary">Primary</a>
+        <a class="button is-link">Link</a>
+      </div>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+<body>
+    <!-- <nav></nav> -->
+    <welcome></welcome>
+    <!-- <footer></footer> -->
+</body>
+</template>
+
+<script>
+// import nav from '../components/nav.vue'
+import welcome from '../components/welcome.vue'
+// import footer from '../components/footer.vue'
+
+export default {
+  name: 'home',
+  components: {
+    welcome,
+  }
+}
+</script>
