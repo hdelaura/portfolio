9eaa78ae2fec41578469beb050fb767a9df17356
Haley DeLaura <d00266543@dmail.dixie.edu>
2021-03-18 16:08:15 -0600

    removed nav from about for single page

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,7 +1,17 @@
 <template>
 <div class="container">
   <section class="section">
-
+    <div class="columns is-mobile">
+      <div class="column is-half">
+        <div>-----   What I Do</div>
+        <h1 class="is-size-1">
+            I enjoy creating elegant,<br>
+            unique, and memorable user<br>
+            experiences.
+        </h1>
+      </div>
+      <div class="column is-half"></div>
+    </div>
   </section>
 </div>
 </template>
--- src/views/about.vue
+++ src/views/about.vue
@@ -1,20 +1,15 @@
 <template>
 <body>
-    <nav></nav>
     <about></about>
-    <!-- <footer></footer> -->
 </body>
 </template>
 
 <script>
-import nav from '../components/nav.vue'
 import about from '../components/about.vue'
-// import footer from '../components/footer.vue'
 
 export default {
   name: 'about',
   components: {
-    nav,
     about
   }
 }
